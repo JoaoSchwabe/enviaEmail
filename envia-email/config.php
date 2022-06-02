3e54e4b57fe301aa10d4a2386a318789d7c26410
João Gabriel Schwabe <81689363+JoaoSchwabe@users.noreply.github.com>
2022-06-02 09:32:44 -0300

    Add files via upload

---
+++ envia-email/config.php
@@ -0,0 +1,9 @@
+<?php
+define("DIR_PATH", "C:\\xampp\htdocs\\envia-email");
+define('URL_BASE', 'http://localhost/envia-email');
+define("URL_TEMPLATE", URL_BASE . "/template");
+
+define("EMAIL_ORIGEM","joaoaulaphpemail@gmail.com");
+define("SENHA_EMAIL","aula123aula");
+define("EMAIL_DESTINO","schwabejoao@gmail.com");
+?>
